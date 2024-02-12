af17650fd161f55b721da25c32d8cb4d3e3d3168
Kevin Escobar <kaemteamo@hotmail.com>
2024-02-12 13:44:01 -0600

    first commit

---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { EstadisticaComponent } from "../ingreso-egreso/estadistica/estadistica.component";
+import { IngresoEgresoComponent } from "../ingreso-egreso/ingreso-egreso.component";
+import { DetalleComponent } from "../ingreso-egreso/detalle/detalle.component";
+
+export const DashboardRoutes: Routes = [
+    {path: 'detalle', component: DetalleComponent},
+    {path: 'ingreso-egreso', component: IngresoEgresoComponent},
+    {path: '', component: EstadisticaComponent},
+]
